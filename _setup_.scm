b8fa103f37e261e87690f7bfcaa910534ec26789
Frederic Hamel <frederic.hamel123@gmail.com>
2019-09-30 16:08:46 -0400

    Add hosted dependency.

---
+++ _setup_.scm
@@ -0,0 +1,8 @@
+
+(define-module-alias (SDL2/ttf) (github.com/frederichamel/SDL2 @0.0.2))
+(define-module-alias (SDL2) (github.com/frederichamel/SDL2 @0.0.2))
+
+;(define-module-alias (SDL2/ttf) @0.0.2)
+;(define-module-alias (SDL2) @0.0.2)
+
+
