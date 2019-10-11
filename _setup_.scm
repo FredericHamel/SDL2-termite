fc7364c7873dba7f19649d8788f4ec45b7847a87
Frederic Hamel <frederic.hamel123@gmail.com>
2019-10-10 23:39:02 -0400

    Update to SDL2@0.0.3

--- _setup_.scm
+++ _setup_.scm
@@ -1,6 +1,6 @@
 
-(define-module-alias (SDL2/ttf) (github.com/frederichamel/SDL2 @0.0.2))
-(define-module-alias (SDL2) (github.com/frederichamel/SDL2 @0.0.2))
+(define-module-alias (SDL2/ttf) (github.com/frederichamel/SDL2 @0.0.3))
+(define-module-alias (SDL2) (github.com/frederichamel/SDL2 @0.0.3))
 
 ;(define-module-alias (SDL2/ttf) @0.0.2)
 ;(define-module-alias (SDL2) @0.0.2)
